658e23bc63d1587d6b539d0ae97cabae70a5db03
Muggle Wei <mugglewei@gmail.com>
2017-02-12 15:51:39 +0800

    Add auto config vscode extensions

---
+++ vscode_config_extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# c/c++
+code --install-extension ms-vscode.cpptools
+# cmake
+code --install-extension twxs.cmake
+# markdown preview
+code --install-extension hnw.vscode-auto-open-markdown-preview
