39b77a3f0e20d2ba831986b4d4f8a6f452d867dc
Muggle Wei <mugglewei@gmail.com>
2017-08-05 23:11:44 +0800

    skip promt in clean boot script

--- system/clean_boot.sh
+++ system/clean_boot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo dpkg --list 'linux-image*'|awk '{ if ($1=="ii") print $2}'|grep -v `uname -r`|grep -v 'extra' > tmp_clean.txt
+sudo dpkg --list 'linux-image*'|awk '{ if ($1=="ii") print $2}'|grep -v `uname -r`|grep -v 'extra'|grep -v 'linux-image-generic' > tmp_clean.txt
 
 while read line
 do
-	echo clean $line
-	sudo apt-get purge $line
-done < clean_tmp.txt
-rm -rf tmp_clean.txt
+	# echo $line
+	sudo apt-get -y purge $line
+done < tmp_clean.txt
 
 sudo apt-get autoremove
 sudo update-grub
+
+rm -rf tmp_clean.txt
