72769f795de3111a908cddb01d86a82045670558
mugglewei <mugglewei@gmail.com>
2020-10-09 22:41:10 +0800

    update libev_ss/client_run.sh

--- shadowsocks/libev_ss/client_run.sh
+++ shadowsocks/libev_ss/client_run.sh
@@ -4,7 +4,7 @@
 sudo cp ./client_config.json /etc/shadowsocks-libev/server0.json
 
 # start client
-sudo systemctl start shadowsocks-libev-local@server0.service
+sudo systemctl restart shadowsocks-libev-local@server0.service
 
 # make sure it's running
 systemctl status shadowsocks-libev-local@server0.service
