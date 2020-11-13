cb8ddb76775fd452db3302a4ffd9de6817a55b6f
mugglewei <mugglewei@gmail.com>
2020-11-13 22:41:58 +0800

    update login_notify

--- system/login_notify.sh
+++ system/login_notify.sh
@@ -5,5 +5,9 @@ IP="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
 HOSTNAME=$(hostname)
 NOW=$(date +"%Y-%m-%dT%H:%M:%S")
 
+# change dir to this script path
+cur_path=$(readlink -f "$(dirname "$0")")
+cd $cur_path
+
 # notify
 echo "$IP $HOSTNAME $NOW"
