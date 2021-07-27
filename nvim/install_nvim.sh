bb2e580594916d5150845c0b4d36abd7f8a5b09c
mugglewei <mugglewei@gmail.com>
2021-07-28 01:28:41 +0800

    update nvim config, tip set proxy in install nvim

--- nvim/install_nvim.sh
+++ nvim/install_nvim.sh
@@ -8,6 +8,11 @@ cd neovim
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 
 # Build
+# NOTE: maybe need proxy in some situation
+# export http_proxy=socks5://127.0.0.1:1080
+# export https_proxy=socks5://127.0.0.1:1080
+# git config --global http.proxy 'socks5://127.0.0.1:1080'
+# git config --global https.proxy 'socks5://127.0.0.1:1080'
 make CMAKE_BUILD_TYPE=Release
 
 # Install
