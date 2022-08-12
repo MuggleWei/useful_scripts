a76a1f14f4ead8139a3718fbf1740f0c2977207e
mugglewei <mugglewei@gmail.com>
2022-08-12 21:54:24 +0800

    add essential_sources

---
+++ essential_sources/essential_sources.py
@@ -0,0 +1,79 @@
+""" 下载必备代码库 """
+import os
+import subprocess
+
+
+def run_subprocess(args):
+    """
+    运行子进程并实时输出
+    :param args: 执行的命令
+    """
+    with subprocess.Popen(args, stdout=subprocess.PIPE) as p:
+        try:
+            for line in p.stdout:
+                if not line:
+                    break
+                print("{}".format(line.decode("utf-8")))
+        except Exception as e:
+            print("{} failed: {}".format(args, e))
+
+
+if __name__ == "__main__":
+    # 需要备份的代码库
+    source_list = [
+        # 自己的代码库
+        "https://github.com/MuggleWei/mugglec.git",
+        "https://github.com/MuggleWei/Hakuna_Matata.git",
+        "https://github.com/MuggleWei/useful_scripts.git",
+        "https://github.com/MuggleWei/mugglewei.github.io.git",
+        "https://github.com/MuggleWei/learning_compass.git",
+        "https://github.com/MuggleWei/babeltrader.git",
+        "https://github.com/MuggleWei/mugglecpp.git",
+        "https://github.com/MuggleWei/latency-benchmark.git",
+
+        # 代码
+        "https://github.com/Kitware/CMake.git",
+        "https://github.com/wolfSSL/wolfssl.git",
+        "https://github.com/openssl/openssl.git",
+        "https://github.com/Mbed-TLS/mbedtls.git",
+        "https://github.com/madler/zlib.git",
+        "https://github.com/mcmilk/7-Zip-zstd.git",
+        "https://github.com/google/googletest.git",
+        "https://github.com/protocolbuffers/protobuf.git",
+        "https://github.com/gflags/gflags.git",
+        "https://github.com/danmar/cppcheck.git",
+        "https://github.com/leethomason/tinyxml2.git",
+        "https://github.com/Tencent/rapidjson.git",
+        "https://github.com/ocornut/imgui.git",
+
+        # 生活
+        "https://github.com/shadowsocks/shadowsocks-libev.git",
+        "https://github.com/adityatelange/hugo-PaperMod.git",
+    ]
+
+    # 当前工作目录下的目录
+    cur_dirs = os.listdir(".")
+
+    # 遍历清单, 获取状态
+    cur_dir = os.getcwd()
+    for src in source_list:
+        name = src.split("/")[-1][:-4]
+        if name in cur_dirs:
+            print("----------------------------------------------")
+            print("{}: already exists, git pull".format(name))
+            # 进入文件夹
+            os.chdir(name)
+
+            # 运行git pull
+            args = ["git", "pull"]
+            run_subprocess(args=args)
+
+            # 返回原目录
+            os.chdir(cur_dir)
+        else:
+            print("----------------------------------------------")
+            print("{}: clone".format(name))
+
+            # 运行git pull
+            args = ["git", "clone", src]
+            run_subprocess(args=args)
